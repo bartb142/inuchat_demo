d6adc0830ebc4976244aef5760461fb9453bbe2a
bartb142 <bartb142@gmail.com>
2025-04-19 20:56:21 +0900

    Add TODO comment to persona_editor.py

--- app/persona_editor.py
+++ app/persona_editor.py
@@ -10,3 +10,4 @@ new_text = st.text_area("現在のペルソナ", value=text, height=300)
 if st.button("保存"):
     save_persona(new_text)
     st.success("ペルソナが保存されました！")
+# TODO: this page is not functioning yet still work in progress but I might delete it since it's a demo app
