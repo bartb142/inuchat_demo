4e46fc8a2e3f6154fd3f58d8bdb075608881d820
bartb142 <bartb142@gmail.com>
2025-05-14 13:08:03 +0900

    Update name field in session state when changing the persona.

--- app/persona_editor.py
+++ app/persona_editor.py
@@ -11,4 +11,5 @@ new_text = st.text_area("現在のペルソナ", value=text, height=300)
 
 if st.button("保存"):
     st.session_state.persona = new_text
+    st.session_state.name = new_text.split('\n')[0].split('：')[1]
     st.success("ペルソナが保存されました！")
